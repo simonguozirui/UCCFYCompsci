7e4f2547263b855e9f594a28f8ac84b284bfa3c9
simonguozirui <simonguozirui@gmail.com>
2017-02-02 11:55:54 -0500

    indentations

--- Java/Algorithm/EvenOdd.java
+++ Java/Algorithm/EvenOdd.java
@@ -2,36 +2,35 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class EvenOdd{
-		public static int[] evenOdd(int[] nums){
-				int [] newarray = new int[nums.length];
-				int evencount = 0;
-				int oddcount = nums.length -1;
-				for (int i = 0; i < nums.length; i++){
-						if (nums[i]%2 == 0){
-								newarray[evencount] = nums[i];
-								evencount = evencount + 1;
-						}else{
-								newarray[oddcount] = nums[i];
-								oddcount = oddcount -1;
-						}		
-		
-				}	
+	public static int[] evenOdd(int[] nums){
+		int [] newarray = new int[nums.length];
+		int evencount = 0;
+		int oddcount = nums.length -1;
+		for (int i = 0; i < nums.length; i++){
+			if (nums[i]%2 == 0){
+				newarray[evencount] = nums[i];
+				evencount = evencount + 1;
+			}else{
+				newarray[oddcount] = nums[i];
+				oddcount = oddcount -1;
+			}		
 
-				return newarray;
+		}	
 
+		return newarray;
+	}	 
 
-
-		}	 
-		public static void main(String[] args){
-				Scanner myscan = new Scanner(System.in);
-				System.out.println("How many elements should be in the array?: ");
-				int numElements = myscan.nextInt();
-				int[] data = new int[numElements];
-				for (int i = 0; i < numElements; i ++){
-						System.out.println("Enter a value to store in element " + 1 + ": ");
-						data[i] = myscan.nextInt();
-				}
-				System.out.println(Arrays.toString(evenOdd(data)));
+	
+	public static void main(String[] args){
+		Scanner myscan = new Scanner(System.in);
+		System.out.println("How many elements should be in the array?: ");
+		int numElements = myscan.nextInt();
+		int[] data = new int[numElements];
+		for (int i = 0; i < numElements; i ++){
+			System.out.println("Enter a value to store in element " + 1 + ": ");
+			data[i] = myscan.nextInt();
 		}
+		System.out.println(Arrays.toString(evenOdd(data)));
+	}
 
 }
