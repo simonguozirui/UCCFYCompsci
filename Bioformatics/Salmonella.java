5b887d1d029a2f5f890c4ce1827b5d2998dd4616
simonguozirui <simonguozirui@gmail.com>
2017-02-16 11:58:14 -0500

    working code that make searches for DNA sequence

--- Bioformatics/Salmonella.java
+++ Bioformatics/Salmonella.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class Salmonella {
-	public static void main(String[] args) {
-		
+	public static boolean findpattern(String pattern, Scanner scanner){
+		boolean returnValue = false;
+		while (scanner.hasNext()){
+			if(scanner.nextLine().indexOf(pattern) > -1){
+				returnValue = true;
+			}
+		}
+		return returnValue;
 	}
 
+	public static void main(String[] args) {
+		try{
+			Scanner myscan = new Scanner(new File("salmonella.txt"));
+			boolean result = findpattern("TTATT",myscan);
+			System.out.println(result);
+		}catch(FileNotFoundException e){
+			System.out.println("Bioformatics data not avaliable");
+		}
+
+	}
 }
